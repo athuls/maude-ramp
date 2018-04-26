1606d8867a721311240571c5b8ff8f01c0ae2dd7
Si Liu <nobi@node0.maude4.confluence.emulab.net>
2018-04-26 11:57:52 -0600

    Latest multiple client/server files with test for socket creation on-demand on server side

--- contrib/YCSB/load-ycsb.sh
+++ contrib/YCSB/load-ycsb.sh
@@ -1,5 +1,5 @@
 rm keyMappings.txt
-bin/ycsb load kaiju -p hosts=155.98.38.115:9810 -p port=9810 -P workloads/workloada_load -p operationcount=1 -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p threadcount=1 -p time=10000 -p valuesize=1 -s
+bin/ycsb load kaiju -p hosts=155.98.38.115:9810,155.98.38.116:9810,155.98.38.106:9810 -p port=9810 -P workloads/workloada_load -p operationcount=1 -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p threadcount=3 -p time=10000 -p valuesize=1 -s
 #bin/ycsb run kaiju -p hosts=127.0.0.1:9910 -p port=9910 -P workloads/workloada -p operationcount=2 -p maxexecutiontime=30 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p threadcount=1 -s
 #bin/ycsb load kaiju -p hosts=127.0.0.1:9810,128.174.244.87:9810 -p port=9810 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p threadcount=2 -s
 #bin/ycsb load kaiju -p hosts=127.0.0.1:9910 -p port=9910 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -postload -s
