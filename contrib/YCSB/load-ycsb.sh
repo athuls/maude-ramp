38a5ab7cfa1827c451478b012785699e68a72859
Si Liu <nobi@node1.maude4.confluence.emulab.net>
2018-04-28 21:25:17 -0600

    Multi-client and multi-server works

--- contrib/YCSB/load-ycsb.sh
+++ contrib/YCSB/load-ycsb.sh
@@ -1,4 +1,4 @@
-rm keyMappings.txt
+rm keyMappings*
 bin/ycsb load kaiju -p hosts=155.98.38.114:9810,155.98.38.99:9810 -p port=9810 -P workloads/workloada_load -p operationcount=1 -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p threadcount=2 -p time=10000 -p valuesize=1 -s
 #bin/ycsb run kaiju -p hosts=127.0.0.1:9910 -p port=9910 -P workloads/workloada -p operationcount=2 -p maxexecutiontime=30 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p threadcount=1 -s
 #bin/ycsb load kaiju -p hosts=127.0.0.1:9810,128.174.244.87:9810 -p port=9810 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p threadcount=2 -s
