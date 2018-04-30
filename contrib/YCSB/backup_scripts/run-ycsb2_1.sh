ff63440cfe0f2bfa25b37ccde80a78fad7010a49
Atul Sandur <nobi@node2.maude4.confluence.emulab.net>
2018-04-30 16:10:08 -0600

    Checkpoint scripts update

--- contrib/YCSB/backup_scripts/run-ycsb2_1.sh
+++ contrib/YCSB/backup_scripts/run-ycsb2_1.sh
@@ -1,7 +1,20 @@
 #rm keyMappings.txt
 #bin/ycsb load kaiju -p hosts=128.174.244.87:9810 -p port=9810 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p threadcount=1 -s
-cp keyMappings.txt keyMappings.bak 
-bin/ycsb run kaiju -p hosts=155.98.38.146:9910 -p port=9910 -P workloads/workloada -p operationcount=100 -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p threadcount=1 -p time=10000 -p valuesize=1
+cd /users/nobi/ramp/maude-ramp/contrib/YCSB
+cp keyMappings.txt keyMappings_bak.bak
+ip=$1
+initial_port=$2
+num_clients=$3
+opcount=$4
+iter=0
+while [ "$iter" -lt "$num_clients" ]
+do 
+	port=`expr $initial_port + $iter` 
+	echo `expr $port`
+	bin/ycsb run kaiju -p hosts=155.98.38.$ip:$port -p port=$port -P workloads/workloada -p operationcount=$opcount -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p threadcount=1 -p time=10000 -p valuesize=1 &
+	iter=`expr $iter + 1`
+done
+
 #bin/ycsb load kaiju -p hosts=127.0.0.1:9810,128.174.244.87:9810 -p port=9810 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p threadcount=2 -s
 #bin/ycsb load kaiju -p hosts=128.174.244.87:9810 -p port=9810 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -postload -s
 #bin/ycsb load kaiju -p hosts=128.174.244.87:9910,127.0.0.1:9910 -p port=9910 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -postload -s
