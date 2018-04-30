a59daed2b9215ea8c997031295f8d1aa93eeb7d6
Atul Sandur <nobi@node2.maude4.confluence.emulab.net>
2018-04-30 16:11:16 -0600

    Automated run phase

--- contrib/YCSB/ycsb_scripts/postload-ycsb.sh
+++ contrib/YCSB/ycsb_scripts/postload-ycsb.sh
@@ -6,29 +6,29 @@ cd /users/nobi/ramp/maude-ramp/contrib/YCSB
 cat keyMappings1* > keyMappings.txt
 if [ "$#" = "1" ]
 then
-	bin/ycsb load kaiju -p hosts=155.98.38.$1:9710 -p port=9710 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p time=10000 -p valuesize=1 -postload -s
+	bin/ycsb load kaiju -p hosts=155.98.38.$1:9710 -p port=9710 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p time=10000 -p valuesize=1 -postload -s &
 elif [ "$#" = "2" ]
 then
-	bin/ycsb load kaiju -p hosts=155.98.38.$1:9710 -p port=9710 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p time=10000 -p valuesize=1 -postload -s
-	bin/ycsb load kaiju -p hosts=155.98.38.$2:9710 -p port=9710 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p time=10000 -p valuesize=1 -postload -s
+	bin/ycsb load kaiju -p hosts=155.98.38.$1:9710 -p port=9710 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p time=10000 -p valuesize=1 -postload -s &
+	bin/ycsb load kaiju -p hosts=155.98.38.$2:9710 -p port=9710 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p time=10000 -p valuesize=1 -postload -s &
 elif [ "$#" = "3" ]
 then
-	bin/ycsb load kaiju -p hosts=155.98.38.$1:9710 -p port=9710 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p time=10000 -p valuesize=1 -postload -s
-	bin/ycsb load kaiju -p hosts=155.98.38.$2:9710 -p port=9710 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p time=10000 -p valuesize=1 -postload -s
-	bin/ycsb load kaiju -p hosts=155.98.38.$3:9710 -p port=9710 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p time=10000 -p valuesize=1 -postload -s
+	bin/ycsb load kaiju -p hosts=155.98.38.$1:9710 -p port=9710 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p time=10000 -p valuesize=1 -postload -s &
+	bin/ycsb load kaiju -p hosts=155.98.38.$2:9710 -p port=9710 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p time=10000 -p valuesize=1 -postload -s &
+	bin/ycsb load kaiju -p hosts=155.98.38.$3:9710 -p port=9710 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p time=10000 -p valuesize=1 -postload -s &
 elif [ "$#" = "4" ]
 then
-	bin/ycsb load kaiju -p hosts=155.98.38.$1:9710 -p port=9710 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p time=10000 -p valuesize=1 -postload -s
-	bin/ycsb load kaiju -p hosts=155.98.38.$2:9710 -p port=9710 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p time=10000 -p valuesize=1 -postload -s
-	bin/ycsb load kaiju -p hosts=155.98.38.$3:9710 -p port=9710 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p time=10000 -p valuesize=1 -postload -s
-	bin/ycsb load kaiju -p hosts=155.98.38.$4:9710 -p port=9710 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p time=10000 -p valuesize=1 -postload -s
+	bin/ycsb load kaiju -p hosts=155.98.38.$1:9710 -p port=9710 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p time=10000 -p valuesize=1 -postload -s &
+	bin/ycsb load kaiju -p hosts=155.98.38.$2:9710 -p port=9710 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p time=10000 -p valuesize=1 -postload -s &
+	bin/ycsb load kaiju -p hosts=155.98.38.$3:9710 -p port=9710 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p time=10000 -p valuesize=1 -postload -s &
+	bin/ycsb load kaiju -p hosts=155.98.38.$4:9710 -p port=9710 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p time=10000 -p valuesize=1 -postload -s &
 elif [ "$#" = "5" ]
 then
-	bin/ycsb load kaiju -p hosts=155.98.38.$1:9710 -p port=9710 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p time=10000 -p valuesize=1 -postload -s
-	bin/ycsb load kaiju -p hosts=155.98.38.$2:9710 -p port=9710 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p time=10000 -p valuesize=1 -postload -s
-	bin/ycsb load kaiju -p hosts=155.98.38.$3:9710 -p port=9710 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p time=10000 -p valuesize=1 -postload -s
-	bin/ycsb load kaiju -p hosts=155.98.38.$4:9710 -p port=9710 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p time=10000 -p valuesize=1 -postload -s
-	bin/ycsb load kaiju -p hosts=155.98.38.$5:9710 -p port=9710 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p time=10000 -p valuesize=1 -postload -s
+	bin/ycsb load kaiju -p hosts=155.98.38.$1:9710 -p port=9710 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p time=10000 -p valuesize=1 -postload -s &
+	bin/ycsb load kaiju -p hosts=155.98.38.$2:9710 -p port=9710 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p time=10000 -p valuesize=1 -postload -s &
+	bin/ycsb load kaiju -p hosts=155.98.38.$3:9710 -p port=9710 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p time=10000 -p valuesize=1 -postload -s &
+	bin/ycsb load kaiju -p hosts=155.98.38.$4:9710 -p port=9710 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p time=10000 -p valuesize=1 -postload -s &
+	bin/ycsb load kaiju -p hosts=155.98.38.$5:9710 -p port=9710 -P workloads/workloada -p operationcount=1 -p maxexecutiontime=30000 -p isolation_level=READ_ATOMIC -p read_atomic_algorithm=KEY_LIST -p time=10000 -p valuesize=1 -postload -s &
 else
 	echo "Fix your input arguments"
 fi
