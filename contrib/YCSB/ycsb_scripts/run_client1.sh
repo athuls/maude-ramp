3709480d4cc082d0ed31844f173492be963b9d93
Atul Sandur <nobi@node1.maude4.confluence.emulab.net>
2018-05-08 17:55:32 -0600

    Introducing delay in run client script to synchronize them to run concurrently

--- contrib/YCSB/ycsb_scripts/run_client1.sh
+++ contrib/YCSB/ycsb_scripts/run_client1.sh
@@ -1,5 +1,24 @@
+# To run: bash run_client1.sh 44 154 17:52:50.12345
 cd /users/nobi/ramp/maude-ramp/contrib/YCSB/maude-middleware/maude_client_side
 rm /run/shm/*
 sed -i -- 's/self = "155.98.38.[0-9]*/self = "155.98.38.'$1'/g' client1.maude
 sed -i -- 's/addr1 = "155.98.38.[0-9]*/addr1 = "155.98.38.'$2'/g' client1.maude
-../maude-binaries/maude.linux64 client1.maude > /run/shm/maude_client1_logs.txt
+
+current_epoch=$(date +%s.%N)
+#target_epoch=$(date -d "17:50:00.12345" +%s.%N)
+target_epoch=$(date -d "$3" +%s.%N)
+
+sleep_seconds=$(echo "$target_epoch - $current_epoch"|bc)
+echo $sleep_seconds
+
+#if [[ $(echo "$sleep_seconds" | bc) -ne $(echo "0" | bc) ]]
+if [[ $(bc -l <<< "$sleep_seconds" > 0) -eq 0 ]]
+then
+	echo "Sleep is fine"
+	echo $sleep_seconds
+	sleep $sleep_seconds
+	../maude-binaries/maude.linux64 client1.maude > /run/shm/maude_client1_logs.txt
+else
+	echo "Sleep time is negative"
+	exit 1
+fi
