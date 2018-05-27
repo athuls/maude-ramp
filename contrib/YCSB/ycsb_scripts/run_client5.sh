094a693dc54e6cb548aedf05a9d087b6dd381e51
Atul Sandur <nobi@node1.maude4.confluence.emulab.net>
2018-05-26 23:29:27 -0600

    Created logs folder

--- contrib/YCSB/ycsb_scripts/run_client5.sh
+++ contrib/YCSB/ycsb_scripts/run_client5.sh
@@ -4,7 +4,7 @@ cd /users/nobi/ramp/maude-ramp/contrib/YCSB/maude-middleware/maude_client_side
 #rm /run/shm/*
 sed -i -- 's/self = "155.98.38.[0-9]*/self = "155.98.38.'$1'/g' client5.maude
 sed -i -- 's/addr1 = "155.98.38.[0-9]*/addr1 = "155.98.38.'$2'/g' client5.maude
-../maude-binaries/maude.linux64 client5.maude > /run/shm/maude_client5_logs.txt
+../maude-binaries/maude.linux64 client5.maude > ../../ycsb_scripts/logs/maude_client5_logs.txt
 
 : <<'END'
 bash ../../ycsb_scripts/sleep_time.sh $3
--- contrib/YCSB/ycsb_scripts/run_client6.sh
+++ contrib/YCSB/ycsb_scripts/run_client6.sh
@@ -4,7 +4,7 @@ cd /users/nobi/ramp/maude-ramp/contrib/YCSB/maude-middleware/maude_client_side
 #rm /run/shm/*
 sed -i -- 's/self = "155.98.38.[0-9]*/self = "155.98.38.'$1'/g' client6.maude
 sed -i -- 's/addr1 = "155.98.38.[0-9]*/addr1 = "155.98.38.'$2'/g' client6.maude
-../maude-binaries/maude.linux64 client6.maude > /run/shm/maude_client6_logs.txt
+../maude-binaries/maude.linux64 client6.maude > ../../ycsb_scripts/logs/maude_client6_logs.txt
 
 : <<'END'
 bash ../../ycsb_scripts/sleep_time.sh $3
