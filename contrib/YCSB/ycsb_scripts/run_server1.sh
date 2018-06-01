0eaa7ea2f1cfbde489300dfdc8cecaed99946079
Atul Sandur <nobi@node1.maude3.confluence.emulab.net>
2018-06-01 15:13:59 -0600

    New maude 116 logs

--- contrib/YCSB/ycsb_scripts/run_server1.sh
+++ contrib/YCSB/ycsb_scripts/run_server1.sh
@@ -1,4 +1,4 @@
 cd /users/nobi/ramp/maude-ramp/contrib/YCSB/maude-middleware/maude_server_side
 rm /run/shm/*
 sed -i -- 's/155.98.38.[0-9]*/155.98.38.'$1'/g' server1.maude
-../maude-binaries/maude.linux64 server1.maude > /run/shm/maude_server1_logs.txt
+../maude-binaries/maude.linux64 server1.maude > ./logs/maude_server1_logs.txt
