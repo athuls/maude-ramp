cf4b15609e606e51d11405e8ccc24b221327e808
Atul Sandur <nobi@node0.maude5.confluence.emulab.net>
2018-07-28 19:42:38 -0600

    Adding script insructions

--- contrib/YCSB/ycsb_scripts/run_server4.sh
+++ contrib/YCSB/ycsb_scripts/run_server4.sh
@@ -1,4 +1,4 @@
 cd /users/nobi/ramp/maude-ramp/contrib/YCSB/maude-middleware/maude_server_side
 rm /run/shm/*
 sed -i -- 's/155.98.38.[0-9]*/155.98.38.'$1'/g' server4.maude
-../maude-binaries/maude.linux64 -interactive server4.maude > /run/shm/maude_server4_logs.txt
+../maude-binaries/alpha117/maude-Yices2.linux64 server4.maude > /proj/Confluence/maude/debug_logs/temp/maude_server4_logs.txt
