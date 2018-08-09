a426b8b06efbd05ddde7325d390b87488b4e6f67
Atul Sandur <athuls89@gmail.com>
2018-08-08 18:20:33 -0700

    Support running multiple maude instances

--- contrib/YCSB/ycsb_scripts/run_client5.sh
+++ contrib/YCSB/ycsb_scripts/run_client5.sh
@@ -1,11 +1,12 @@
 # To run: bash run_client5.sh 44 154 17:52:50.12345
 #!/bin/bash
 cd /users/nobi/ramp/maude-ramp/contrib/YCSB/maude-middleware/maude_client_side
-sed -i -- 's/self = "155.98.38.[0-9]*/self = "155.98.38.'$1'/g' init-client5.maude
 if [ $# == 2 ] 
 then
+	sed -i -- 's/self = "155.98.38.[0-9]*/self = "155.98.38.'$1'/g' init-client5_$2.maude
 	../maude-binaries/alpha117/maude-Yices2.linux64 init-client5_$2.maude > /proj/Confluence/maude/debug_logs/temp/maude_client5_logs_$2.txt
 else
+	sed -i -- 's/self = "155.98.38.[0-9]*/self = "155.98.38.'$1'/g' init-client5.maude
 	../maude-binaries/alpha117/maude-Yices2.linux64 init-client5.maude > /proj/Confluence/maude/debug_logs/temp/maude_client5_logs.txt
 fi
 
