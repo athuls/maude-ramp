29345478aa785ae2003a5d3d8e087a7428c70588
Si Liu <uiucnobi@gmail.com>
2018-08-11 13:14:01 -0500

    Add files via upload

--- contrib/YCSB/ycsb_scripts/run_server0.sh
+++ contrib/YCSB/ycsb_scripts/run_server0.sh
@@ -1,4 +1,4 @@
 cd /users/nobi/ramp/maude-ramp/contrib/YCSB/maude-middleware/maude_server_side
 rm /run/shm/*
 sed -i -- 's/155.98.38.[0-9]*/155.98.38.'$1'/g' init-server0.maude
-../maude-binaries/alpha117/maude-Yices2.linux64 init-server0.maude > /proj/Confluence/maude/debug_logs/temp/maude_server0_logs.txt
+../../../../../../alpha118/maude init-server0.maude >& /proj/Confluence/maude/debug_logs/temp/maude_server0_logs.txt
