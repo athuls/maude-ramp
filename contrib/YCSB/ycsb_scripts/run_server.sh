f35646cab2bcb0711279f224fd9fdee8fd1dd00f
Si Liu <uiucnobi@gmail.com>
2018-12-21 05:08:02 -0600

    Update run_server.sh

--- contrib/YCSB/ycsb_scripts/run_server.sh
+++ contrib/YCSB/ycsb_scripts/run_server.sh
@@ -29,8 +29,9 @@ done
 # End of updating server file
 
 # Clean up server logs directory
-rm -rf /proj/Confluence/maude/debug_logs/temp/server_logs"$2"
-mkdir /proj/Confluence/maude/debug_logs/temp/server_logs"$2"
+#rm -rf /proj/Confluence/maude/debug_logs/temp/server_logs"$2"
+#mkdir /proj/Confluence/maude/debug_logs/temp/server_logs"$2"
+rm -rf /proj/Confluence/maude/debug_logs/temp/server_logs/*
 
 # Run server
 ../maude-binaries/alpha118/maude init-server$2.maude > /proj/Confluence/maude/debug_logs/temp/server_logs/maude_server$2_logs.txt 2>&1
